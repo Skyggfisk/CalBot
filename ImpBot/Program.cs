b355b34991a619fb0850eacc8a07b0779a741252
Skyggfisk <mahasi93@hotmail.com>
2017-02-11 18:07:33 +0100

    Updated packages. First attempt at async Main. Need fix for console app.

--- ImpBot/Program.cs
+++ ImpBot/Program.cs
@@ -4,13 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Discord;
+using Discord.WebSocket;
+
 namespace ImpBot
 {
     class Program
     {
-        static void Main(string[] args)
+        // Convert our sync-main to an async main method
+        static void Main(string[] args) => new Program().Run().GetAwaiter().GetResult();
+
+        // Create a DiscordClient with WebSocket support
+        private DiscordSocketClient client;
+
+        public async Task Run()
         {
-            ImpBot Bot = new ImpBot();
+            client = new DiscordSocketClient();
+
+            // Place the token of your bot account here
+            string token = "Mjc5MzY4MzkzOTczNDMyMzIx.C356Ag.JedWUMyPF2tGY4lOPX0vc3KOwgE";
+
+            // Hook into the MessageReceived event on DiscordSocketClient
+            client.MessageReceived += async (message) =>
+            {   // Check to see if the Message Content is "!ping"
+                if (message.Content == "!ping")
+                    // Send 'pong' back to the channel the message was sent in
+                    await message.Channel.SendMessageAsync("pong");
+            };
+
+            // Configure the client to use a Bot token, and use our token
+            await client.LoginAsync(TokenType.Bot, token);
+            // Connect the client to Discord's gateway
+            await client.ConnectAsync();
+
+            // Block this task until the program is exited.
+            await Task.Delay(-1);
         }
     }
 }

ea8ac6dc25eb2b44242761559ecdaca30cd5e2c9
Skyggfisk <mahasi93@hotmail.com>
2017-02-13 22:08:38 +0100

    Better greet... not much, but a start.

--- ImpBot/ImpCommandModule.cs
+++ ImpBot/ImpCommandModule.cs
@@ -11,12 +11,19 @@ namespace ImpBot
 {
     public class ImpCommandModule : ModuleBase
     {
-        // ~say hello -> hello
+        // !say hello -> hello
         [Command("say"), Summary("Echos a message.")]
         public async Task Say([Remainder, Summary("The text to echo")] string echo)
         {
             // ReplyAsync is a method on ModuleBase
             await ReplyAsync(echo);
+        }
+
+        [Command("greet"), Summary("Message when greeted."), Alias("hi", "hello", "greetings", "gr")]
+        public async Task Greet()
+        {
+            IUser user = Context.Message.Author;
+            await ReplyAsync($"Hello {user.Username}!");
         }
     }
 
@@ -24,7 +31,7 @@ namespace ImpBot
     [Group("sample")]
     public class Sample : ModuleBase
     {
-        // ~sample square 20 -> 400
+        // !sample square 20 -> 400
         [Command("square"), Summary("Squares a number.")]
         public async Task Square([Summary("The number to square.")] int num)
         {
@@ -32,12 +39,12 @@ namespace ImpBot
             await Context.Channel.SendMessageAsync($"{num}^2 = {Math.Pow(num, 2)}");
         }
 
-        // ~sample userinfo --> foxbot#0282
-        // ~sample userinfo @Khionu --> Khionu#8708
-        // ~sample userinfo Khionu#8708 --> Khionu#8708
-        // ~sample userinfo Khionu --> Khionu#8708
-        // ~sample userinfo 96642168176807936 --> Khionu#8708
-        // ~sample whois 96642168176807936 --> Khionu#8708
+        // !sample userinfo --> foxbot#0282
+        // !sample userinfo @Khionu --> Khionu#8708
+        // !sample userinfo Khionu#8708 --> Khionu#8708
+        // !sample userinfo Khionu --> Khionu#8708
+        // !sample userinfo 96642168176807936 --> Khionu#8708
+        // !sample whois 96642168176807936 --> Khionu#8708
         [Command("userinfo"), Summary("Returns info about the current user, or the user parameter, if one passed.")]
         [Alias("user", "whois")]
         public async Task UserInfo([Summary("The (optional) user to get info for")] IUser user = null)
